d3bdfe5a2148c38b873b6b4f6e0f94749cd16798
Matthew Miles <matthew.miles@sight-sound.com>
2024-03-10 07:15:09 -0500

    Updated script

--- OSDCloud/create-OSDCloueWinPEImage.ps1
+++ OSDCloud/create-OSDCloueWinPEImage.ps1
@@ -8,65 +8,30 @@ This script is designed to facilitate the automated update and configuration of 
 
 .NOTES
 Author: Matthew Miles
-Created: August 11, 2023
+Created: March 10, 2024
 Version: 1.0
 
-.EXAMPLE
-.\create-OSDCloudWinPEImage.ps1
-# Prompts the user to check and update the OSD module, then proceeds to configure OSDCloud templates, workspaces, and boot images.
 #>
 
 $brand = 'Sight & Sound Theatres'
 $wallpaperPath = "E:\SSWallpaper2017_1920x1080.jpg"
 
-function Update-OSDModule {
-    [CmdletBinding()]
-    param ()
 
-    # Check admin privileges
-    if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-        Throw "This function requires administrator privileges."
-    }
+$choice = Read-Host "Would you like to check update your PowerShell modules? (Yes(Y) / No(N) / Cancel(C))"
 
-    $ModuleName = "OSD"
-
-    Write-Host "Checking OSD module status..." -ForegroundColor Green
-
-    # Check if OSD module is installed
-    $IsInstalled = Get-InstalledModule -Name $ModuleName -ErrorAction SilentlyContinue
-    if (-not $IsInstalled) {
-        Write-Host "OSD module is not installed. Installing the latest version..." -ForegroundColor Green
-        Install-Module -Name $ModuleName -Scope CurrentUser -Force -AllowClobber
-        Write-Host "OSD module is now installed." -ForegroundColor Green
-        return
-    }
-
-    # Get the installed version of OSD module
-    $InstalledVersion = $IsInstalled.Version
-
-    # Get the latest version of OSD module
-    $LatestVersion = (Find-Module -Name $ModuleName -AllVersions | Sort-Object Version -Descending)[0].Version
-
-    # Check if the installed version is outdated
-    if ($InstalledVersion -lt $LatestVersion) {
-        Write-Host "The installed version of OSD module ($InstalledVersion) is outdated. Updating to the latest version ($LatestVersion)..." -ForegroundColor Green
-        Update-Module -Name $ModuleName -Scope CurrentUser -Force -AllowClobber -ErrorAction Stop
-
-        # Uninstall old versions
-        $OldVersions = Get-InstalledModule -Name $ModuleName | Where-Object { $_.Version -ne $LatestVersion }
-        foreach ($OldVersion in $OldVersions) {
-            Write-Host "Uninstalling old version $($OldVersion.Version) of OSD module..." -ForegroundColor Gray
-            Uninstall-Module -Name $ModuleName -Version $OldVersion.Version -Force -ErrorAction SilentlyContinue
-        }
-
-        Write-Host "OSD module has been updated to version $LatestVersion. Old versions have been uninstalled." -ForegroundColor Green
-    } else {
-        Write-Host "OSD module is up to date ($InstalledVersion)." -ForegroundColor Gray
-    }
+if ($choice -eq "y" -or $choice -eq "yes") {
+    Write-Host -ForegroundColor Green "[+] Updating all PowerShell modules"
+    Invoke-Expression (Invoke-RestMethod -uri "https://raw.githubusercontent.com/1eyeITguy/Cloud_Scipts/main/Modules/Update-ModuleVersions.ps1")    
+} elseif ($choice -eq "n" -or $choice -eq "no") {
+    Write-Host -ForegroundColor Yellow "[-] Not updating modules, continuing with the rest of the script..."
+} elseif ($choice -eq "c" -or $choice -eq "cancel") {
+    Write-Warning "Script execution canceled."
+    Exit
+} else {
+    Write-Warning "Invalid choice. Please select 'y'/'yes', 'n'/'no', or 'c'/'cancel'."
 }
 
-
-function Remove-template_worspace {
+function Remove-Workspaces {
     [CmdletBinding()]
     param ()
 
@@ -76,57 +41,16 @@ $directoriesToDelete = @("C:\ProgramData\OSDCloud", "C:\OSDCloud")
 
 foreach ($dir in $directoriesToDelete) {
     if (Test-Path -Path $dir -PathType Container) {
-        Write-Host -ForegroundColor Cyan "Deleting directory: $dir"
+        Write-Host -ForegroundColor DarkGray "Deleting directory: $dir"
         Remove-Item -Path $dir -Recurse -Force
-        Write-Host -ForegroundColor DarkGreen "Directory deleted: $dir"
     } else {
-        Write-Host -ForegroundColor Yellow "Directory not found: $dir"
+        Write-Warning "Directory not found: $dir"
     }
 }
 }
 
-
-
-function Update-WDSBootImage {
-    # Import the WDS module if not already loaded
-    Import-Module WDS
-
-    # Name of the boot image to remove and add
-    $bootImageName = "OSDCloud"
-    $bootImagePath = "C:\OSDCloud\Media\sources\boot.wim"
-    $bootImageDescription = "Updated OSDCloud Boot Image $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
-
-    # Get the boot image to remove
-    $existingBootImage = Get-WdsBootImage | Where-Object { $_.ImageName -eq $bootImageName }
-
-    # Remove the existing boot image if found
-    if ($existingBootImage -ne $null) {
-        Remove-WdsBootImage -ImageName $bootImageName -Architecture x64
-        Write-Host -ForegroundColor DarkGray "Boot image '$bootImageName' has been removed."
-    }
-
-    # Import the new boot image
-    $importedBootImage = Import-WdsBootImage -Path $bootImagePath -NewImageName $bootImageName -NewDescription $bootImageDescription -DisplayOrder 1
-    if ($importedBootImage -ne $null) {
-        Write-Host -ForegroundColor Cyan "Boot image '$bootImageName' has been imported."
-    }    
-}
-
-
-$choice = Read-Host "Would you like to check for and update the OSD module? (y/n/c)"
-
-if ($choice -eq "y" -or $choice -eq "yes") {
-    Update-OSDModule
-} elseif ($choice -eq "n" -or $choice -eq "no") {
-    Write-Host "Continuing with the rest of the script..."
-} elseif ($choice -eq "c" -or $choice -eq "cancel") {
-    Write-Host "Script execution canceled."
-    Exit
-} else {
-    Write-Host "Invalid choice. Please select 'y'/'yes', 'n'/'no', or 'c'/'cancel'."
-}
-
-Remove-template_worspace
+# Remove old workspaces and templates
+Remove-Workspaces
 
 # Create new OSDCloud template
 New-OSDCloudTemplate
@@ -134,7 +58,80 @@ New-OSDCloudTemplate
 # Create new OSDCloud Workspace
 New-OSDCloudWorkspace
 
-# Edit the WinPE image with branding and drivers
+# Edit the WinPE image with branding and all drivers
 Edit-OSDCloudWinPE -StartOSDCloudGUI -Brand $brand -CloudDriver * -Wallpaper $wallpaperPath
 
-Update-WDSBootImage
+# Set OSDCloudGUI Defaults for the PXE boot image
+# Set the path to the boot.wim file
+$BootWimPath = "C:\OSDCloud\Media\sources\boot.wim"
+$MountPath = "C:\Mount"
+$DestinationPath = "$MountPath\OSDCloud\Automate"
+
+# Check if the $MountPath directory exists
+if (-not (Test-Path -Path $MountPath)) {
+    # Create the directory
+    New-Item -ItemType Directory -Path $MountPath
+    Write-Host -ForegroundColor Green "[+] Directory $MountPath has been created."
+} else {
+    Write-Host -ForegroundColor Green "[+] Directory $MountPath already exists."
+}
+
+# Mount the boot.wim file
+Mount-WindowsImage -ImagePath $BootWimPath -Index 1 -Path $MountPath
+
+# Set OSDCloudGUI Defaults
+$Global:OSDCloud_Defaults = [ordered]@{
+BrandName = $brand
+BrandColor = "Orange"
+OSActivation = "Retail"
+OSEdition = "Pro"
+OSLanguage = "en-us"
+OSImageIndex = 9
+OSName = "Windows 11 23H2 x64"
+OSReleaseID = "23H2"
+OSVersion = "Windows 11"
+OSActivationValues = @(
+"Volume",
+"Retail"
+)
+OSEditionValues = @(
+"Enterprise",
+"Pro"
+)
+OSLanguageValues = @(
+"en-us"
+)
+OSNameValues = @(
+"Windows 11 23H2 x64",
+"Windows 10 22H2 x64"
+)
+OSReleaseIDValues = @(
+"23H2"
+)
+OSVersionValues = @(
+"Windows 11",
+"Windows 10"
+)
+captureScreenshots = $false
+ClearDiskConfirm = $false
+restartComputer = $true
+updateDiskDrivers = $true
+updateFirmware = $true
+updateNetworkDrivers = $true
+updateSCSIDrivers = $true
+}
+
+# Convert the OSDCloud_Defaults variable to JSON format
+$OSDCloud_Defaults_JSON = $Global:OSDCloud_Defaults | ConvertTo-Json
+
+# Create 'Start-OSDCloudGUI.json' within the mounted image
+$OSDCloudGUIjson = New-Item -Path "$DestinationPath\Start-OSDCloudGUI.json" -Force
+
+# Write the contents of the OSDCloud_Defaults_JSON variable to the Start-OSDCloudGUI.json file
+Set-Content -Path $OSDCloudGUIjson.FullName -Value $OSDCloud_Defaults_JSON
+
+# Save changes and unmount the image
+Dismount-WindowsImage -Path $MountPath -Save
+
+# Clean up the C:\Mount directory by deleting all files in it
+Remove-Item -Path "$MountPath\*" -Recurse -Force
